5f51e256894fafd3c47f2339f3334df2606bf436
rmazoouqur <marzooq697231@gmail.com>
2025-02-17 01:22:43 +0530

    add dates to bloglist

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -5,11 +5,15 @@ const BlogList = () => {
   const allPosts = getAllPosts()
 
   return (
-    <ul className="mx-auto">
+    <ul className="mt-10">
       {allPosts.map((i) => {
         return (
           <Link  key={i.slug} href={`blog/${i.slug}`}>
-            <li>{i.title}</li>
+            <li className="mx-auto w-1/2">
+              <h1 className="text-3xl mb-4">{i.title}</h1>
+              <p className="font-thin text-sm">{i.date}</p>
+              <hr />
+            </li>
           </Link>
         )
       })}
