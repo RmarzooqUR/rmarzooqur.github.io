9c4afdd7e4ec56f8988c128d448d71e4ea04d001
rmarzooqur <marzooq697231@gmail.com>
2026-02-25 13:06:05 +0530

    add glass variant navbar & style code blocks

---
+++ src/components/NavBar/VariantGlass.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import styles from './styles.module.css'
+
+export default function VariantGlass() {
+  return <div className={`${styles.nav_container}`}>
+      <div className={`${styles.glass} py-8 px-20 flex flex-row justify-between`}>
+        <div className="logo">
+          <Link href="/" className="mx-2 hover:opacity-35">Logo</Link>
+        </div>
+        <div className="nav flex flex-row justify-center">
+
+          <Link href="/" className="mx-2 hover:opacity-35">
+            Home
+          </Link >
+          <Link href="" className="mx-2 hover:opacity-35">
+            Tools
+          </Link >
+          <Link href="/blog" className="mx-2 hover:opacity-35">
+            Blog
+          </Link >
+          <Link href="" className="mx-2 hover:opacity-35">
+            Contact
+          </Link >
+        </div>
+      </div>
+    </div>
+}
--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,27 +1,14 @@
 import Link from "next/link";
 import styles from './styles.module.css'
+import VariantGlass from "./VariantGlass";
 
-export default function NavBar() {
-  return <div className={`${styles.nav_container}`}>
-      <div className={`${styles.glass} py-8 px-20 flex flex-row justify-between`}>
-        <div className="logo">
-          <Link href={'/'} className="mx-2 hover:opacity-35">Logo</Link>
-        </div>
-        <div className="nav flex flex-row justify-center">
+type Props = {
+  variant?: string
+}
 
-          <Link href="/" className="mx-2 hover:opacity-35">
-            Home
-          </Link >
-          <Link href="" className="mx-2 hover:opacity-35">
-            Resources
-          </Link >
-          <Link href="/blog" className="mx-2 hover:opacity-35">
-            Blog
-          </Link >
-          <Link href="" className="mx-2 hover:opacity-35">
-            Contact
-          </Link >
-        </div>
-      </div>
-    </div>
+export default function NavBar({variant}: Props) {
+  if (variant === 'glass') {
+    return <VariantGlass />
+  }
+  return <VariantGlass />
 }
