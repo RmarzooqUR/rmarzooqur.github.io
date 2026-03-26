ec4eaef76f7272ea83cece85ff5d521d8c37d0ac
rmarzooqur <marzooq697231@gmail.com>
2026-03-26 13:44:41 +0530

    flex to grid on RecentBlogs, SUBGRID

--- src/components/Home/RecentBlogs/index.tsx
+++ src/components/Home/RecentBlogs/index.tsx
@@ -10,18 +10,20 @@ export const RecentBlogs = ({ posts }: Props) => {
   return <div className=" p-5 recent-blogs h-auto flex items-center flex-col">
     {/* bg-[#FFB7C3] */}
     <h1 className='text-4xl'>From the blog</h1>
-    <div className="flex justify-between flex-col md:flex-row col-auto  ">
+    <div className="gap-2 grid-rows-[2 1fr 2fr] grid-cols-3 grid">
       {posts.map((p) => {
-        return (<div className="md:w-1/3 py-5 md:px-2" key={p.slug}>
-          <Link href={`/blog/${p.slug}`} target='_blank'>
-            <h1 className="text-2xl">{
-              `${p.title.length > TITLE_FORMAT_SLICE
-                ? p.title.slice(0, TITLE_FORMAT_SLICE) + '...'
-                : p.title}`}
-            </h1>
-            <p >{p.summary.slice(0, 100) +'...'}</p>
+        return (
+          <Link href={`/blog/${p.slug}`} className="grid grid-rows-subgrid row-span-2" target='_blank'>
+            <div className="py-5 md:px-2 grid row-span-2 grid-rows-subgrid" key={p.slug}>
+              <h1 className="text-2xl row-start-1">{
+                `${/*p.title.length > TITLE_FORMAT_SLICE
+                  ? p.title.slice(0, TITLE_FORMAT_SLICE) + '...'
+                  :*/ p.title}`}
+              </h1>
+              <p className="row-start-2">{p.summary.slice(0, 250) + '...'}</p>
+            </div>
           </Link>
-        </div>)
+        )
       })}
     </div>
   </div>
