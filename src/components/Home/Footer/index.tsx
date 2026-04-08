02a3b3f0912f70056615f82ae3969fb0018e9c92
rmarzooqur <marzooq697231@gmail.com>
2026-04-08 13:17:25 +0530

    finish subgrid blog

--- src/components/Home/Footer/index.tsx
+++ src/components/Home/Footer/index.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 import styles from './styles.module.css'
 export const Footer: FC = () => {
   return (
-    <div className={`${styles.footer} bottom-0 relative h-auto px-5 py-8`}>
+    <div className={`${styles.footer} bottom-0 relative h-auto px-5 py-8 mt-20`}>
       <h1 className='text-4xl'>Contact Us</h1>
       <p>Fill form for services</p>
       <p><a href="https://storyset.com/technology">Technology illustrations by Storyset</a></p>
